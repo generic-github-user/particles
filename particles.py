8ab183a8606d722891217687add78d7abdbfa7c5
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-06-11 06:43:36 -0400

    add simple particle simulation + rendering

--- particles.py
+++ particles.py
@@ -1,10 +1,39 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-particles = np.random.randint(0, 100, [100, 2])
-canvas = np.zeros([100, 100])
+particles = np.random.randint(0, 100, [100, 2]).astype(float)
+
 # canvas[np.ix_(*particles.T)] = 1
-canvas[np.arange(canvas.shape[0])[:, None], particles] = 1
+
 # print(np.mgrid[particles.T])
-plt.imshow(canvas)
+
+# plt.imshow(canvas)
+# plt.show()
+
+# plt.axis([0, 10, 0, 1])
+
+center = [50.] * 2
+g = 20
+
+for i in range(200):
+    for j in range(100):
+        p = particles[j]
+        d = center - p
+        r = np.clip(np.linalg.norm(d), 5, 10e3)
+        particles[j] += d * (g / (r ** 2))
+
+    particles = particles % 100
+
+    canvas = np.zeros([100, 100])
+    # canvas[np.arange(canvas.shape[0])[:, None], particles.astype(int)] = 1
+    for j in range(100):
+        p = particles[j].astype(int)
+        index = tuple(p)
+        canvas[index] = 1
+
+    plt.clf()
+    plt.imshow(canvas)
+    plt.pause(0.01)
+    plt.axis('off')
+
 plt.show()
