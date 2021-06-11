7647d58725eacf0b6bee22dd433f782b8a1dce08
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-06-11 08:22:14 -0400

    add animation rendering with celluloid module

--- particles.py
+++ particles.py
@@ -1,9 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib.animation import PillowWriter
+from celluloid import Camera
 
-num = 1000
+num = 100
 resolution = 200
 frames = 200
+g = 1
 
 particles = np.random.randint(0, resolution, [num, 2]).astype(float)
 velocity = np.zeros([num, 2], dtype=float)
@@ -18,8 +22,10 @@ velocity = np.zeros([num, 2], dtype=float)
 # plt.axis([0, 10, 0, 1])
 
 center = [resolution / 2.] * 2
-g = 1
+fig, ax = plt.subplots()
+sequence = []
 
+camera = Camera(fig)
 for i in range(frames):
     for j in range(num):
         p = particles[j]
@@ -38,9 +44,33 @@ for i in range(frames):
         index = tuple(p)
         canvas[index] = 1
 
-    plt.clf()
-    plt.imshow(canvas)
-    plt.pause(0.01)
-    plt.axis('off')
+    frame = ax.imshow(canvas, animated=True)
+    sequence.append([frame])
+    camera.snap()
+    print('Rendered frame {}'.format(i+1))
 
-plt.show()
+    # plt.clf()
+    # plt.imshow(canvas)
+    # plt.pause(0.01)
+    # plt.axis('off')
+
+# plt.show()
+
+ani = animation.ArtistAnimation(fig, sequence, interval=10, blit=True, repeat_delay=0)
+
+# To save the animation, use e.g.
+#
+# ani.save("movie.mp4")
+#
+# or
+#
+
+# writer = animation.FFMpegWriter(fps=15, metadata=dict(artist='Me'), bitrate=1800)
+# writer = PillowWriter(fps=20, codec='libx264', bitrate=20)
+# writer.setup(fig, "./test-movie.gif", dpi=100)
+# ani.save("test-movie.gif", writer=writer, dpi=100)
+
+anim = camera.animate()
+anim.save('animation.gif', writer='PillowWriter', fps=20, dpi=100, bitrate=10000)
+
+# plt.show()
